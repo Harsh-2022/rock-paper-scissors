aa6a6ab2ebde7bfbfb456c54f9a9f59c2db3d0b6
Harsh Mittal <harshmittal20feb@gmail.com>
2024-10-28 05:38:39 +0530

    BASIC UI added

--- app.js
+++ app.js
@@ -9,39 +9,45 @@ function getCompChoice() {
   if (r === 2) return "scissors";
 }
 
-function getUserChoice() {
-  return prompt("Enter your choice : Rock, paper or scissors");
-}
+const user = document.querySelector(".user");
+const comp = document.querySelector(".computer");
 
-function playRound() {
-  let human = getUserChoice().toLowerCase();
-  let comp = getCompChoice();
-  console.log(`USER: ${human}`);
-  console.log(`comp: ${comp}`);
+const reset = () => {
+  userScore = 0;
+  compScore = 0;
 
-  if (human === "rock") {
-    if (comp === "scissors") userScore++;
-    if (comp === "paper") compScore++;
+  user.innerText = `Your Score: ${userScore}`;
+  comp.innerText = `Computer's Score: ${compScore}`;
+};
+
+const userdiv = document.querySelector(".userchoice");
+userdiv.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    const human = e.target.className;
+    const compt = getCompChoice();
+    console.log(compt);
+    if (human === "rock") {
+      if (compt === "scissors") userScore++;
+      if (compt === "paper") compScore++;
+    }
+    if (human === "scissors") {
+      if (compt === "paper") userScore++;
+      if (compt === "rock") compScore++;
+    }
+    if (human === "paper") {
+      if (compt === "rock") userScore++;
+      if (compt === "scissors") compScore++;
+    }
+
+    user.innerText = `Your Score: ${userScore}`;
+    comp.innerText = `Computer's Score: ${compScore}`;
+
+    if (userScore === 5) {
+      alert("YOU WIN !!!");
+      reset();
+    } else if (compScore === 5) {
+      alert("YOU LOSE !!!");
+      reset();
+    }
   }
-  if (human === "scissors") {
-    if (comp === "paper") userScore++;
-    if (comp === "rock") compScore++;
-  }
-  if (human === "paper") {
-    if (comp === "rock") userScore++;
-    if (comp === "scissors") compScore++;
-  }
-}
-
-function playGame() {
-  for (let i = 1; i <= 5; i++) playRound();
-  let winner =
-    userScore > compScore
-      ? "USER WINS"
-      : userScore < compScore
-      ? "COMPUTER WINS"
-      : "DRAW";
-  console.log(winner);
-}
-
-playGame();
+});
